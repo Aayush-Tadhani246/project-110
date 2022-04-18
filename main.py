a5287a8de5a60a477e53b85b4b341c459428a2a6
Aayush-Tadhani246 <70973409+Aayush-Tadhani246@users.noreply.github.com>
2022-04-17 21:04:59 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,41 @@
+import csv
+import pandas as pd
+import plotly.figure_factory as ff
+import statistics 
+import random
+import plotly.graph_objects as go
+
+df = pd.read_csv('newdata.csv')
+data = df ['average'].tolist()
+# fig = ff.create_distplot([data],['average'],show_hist=False)
+# fig.show()
+population_mean = statistics.mean(data)
+print("population mean ",population_mean)
+
+def random_set_of_mean(counter):
+    dataset = []
+    for i in range(0,counter):
+        random_index = random.randint(0,len(data)-1)
+        value = data[random_index]
+        dataset.append(value)
+    mean = statistics.mean(dataset)
+    return mean 
+
+def show_fig(mean_list):
+    df = mean_list
+    mean = statistics.mean(df)
+    fig = ff.create_distplot([df],['average'],show_hist=False)
+    fig.add_trace(go.Scatter(x=[mean,mean],y=[0,1],mode='lines',name = 'mean'))
+    fig.show()
+
+def setup():
+    mean_list = []
+    for i in range (0,1000):
+        set_of_mean = random_set_of_mean(100)
+        mean_list.append(set_of_mean)
+    show_fig(mean_list)
+    mean = statistics.mean(mean_list)
+    std = statistics.stdev(mean_list)
+    print("Mean of sampling distrabution ",mean )
+    print("Standered Deveation of sampling distrabution",std)
+setup()
